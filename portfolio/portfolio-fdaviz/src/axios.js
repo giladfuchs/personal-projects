44c7394e128af355468a0c8b6294c3b76bfce405
instalob <instalob@mail.com>
2022-03-05 18:37:51 +0200

    first commit

--- portfolio/portfolio-fdaviz/src/axios.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-  baseURL: "https://portfolio-a42f1.firebaseio.com/",
-});
-
-export default instance;
--- portfolio/portfolio-fdaviz/src/components/Spinner.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const spinner = (props) => {
-  props.change();
-
-  return <div className="loader">Loading...</div>;
-};
-export default spinner;
